9172d14451ef56d781f88d68fcc45fa7a1410538
Krzysztof Jendrzyca <kjendrzyca@future-processing.com>
2016-03-19 19:00:08 +0100

    sending new order to server

--- src/store/ordersActions.js
+++ src/store/ordersActions.js
@@ -1,3 +1,14 @@
 export function addNewOrder (order) {
-    return {type: 'ADD_NEW_ORDER', order: order};
+    return dispatch => {
+        fetch('/api/order', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        }).then(() => {
+            dispatch({type: 'ADD_NEW_ORDER', order: order});
+        });
+    };
 }
