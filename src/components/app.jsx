6d99971d29271e47c2beb8f45b4fe23b188ff16e
Krzysztof Jendrzyca <kjendrzyca@future-processing.com>
2016-03-19 13:25:44 +0100

    adding redux with react router and dummy components / upgrading eslint config

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,56 @@
 import React from 'react';
 import DOM from 'react-dom';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {Router, Route, Link, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
 
-DOM.render(<div>Korposzury</div>, document.getElementById('main-container'));
+import userData from '../store/userData';
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+    combineReducers({
+        userData: userData,
+        routing: routerReducer
+    })
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+const Korposzury = React.createClass({
+    render () {
+        return <div>
+            <Link to="/ala">ala</Link>
+            Korposzury
+        </div>;
+    }
+});
+
+const Ala = React.createClass({
+    render () {
+        return (
+            <div>
+                <button onClick={() => {
+                    this.props.dispatch({type: 'CHANGE_NAME', name: 'krzys'});
+                }}>
+                    zmien imie
+                </button>
+                {this.props.userData.name}
+            </div>
+        );
+    }
+});
+
+const ConnectedAla = connect(
+    state => ({userData: state.userData})
+)(Ala);
+
+DOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Korposzury} />
+            <Route path="/ala" component={ConnectedAla} />
+        </Router>
+    </Provider>,
+    document.getElementById('main-container')
+);
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const PUBLIC_PATH = path.resolve(__dirname, 'public');
 
 app.use('/public', express.static(PUBLIC_PATH));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
