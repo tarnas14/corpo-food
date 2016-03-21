acd40bf8c8a15822e1680007d5aedf0151440671
Grzegorz Lada <grzegorz.lada@gmail.com>
2016-03-21 18:51:36 +0100

    Fixed hour validation.

--- src/components/newOrderForm.jsx
+++ src/components/newOrderForm.jsx
@@ -34,9 +34,22 @@ const NewOrderForm = React.createClass({
         });
     },
 
-    validateHour (hour) {
-        const pattern = /[0-9]{1,2}\:[0-9]{2}/;
-        return pattern.test(hour);
+    validateHour (hourInput) {
+        const pattern = /([0-9]{1,2})\:([0-9]{2})/;
+        if (!pattern.test(hourInput)) {
+            return false;
+        }
+
+        const matches = pattern.exec(hourInput);
+        const hour = matches[1];
+        const minutes = matches[2];
+
+        if (hour > 24 || minutes > 59) {
+            return false;
+        }
+
+        return true;
+
     },
 
     handleTextChange (event) {
