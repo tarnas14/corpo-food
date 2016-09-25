b74e0f21a799781924c524c38956fcfa6bee0f1b
tarnas14 <tarnas14@gmail.com>
2016-09-25 23:37:19 +0200

    moved all dependencies needed to build a project to `dependencies` in package.json; fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './src/public/'),
         filename: '[name].js',
-        publichPath: '/public/'
+        publicPath: '/public/'
     },
 
     module: {
