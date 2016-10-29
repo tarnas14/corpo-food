52b8785c8edb2bcee6ae3d574aacfc6cd7d471fa
kjendrzyca <kjendrzyca@gmail.com>
2016-10-29 16:25:22 +0200

    Updated packages / added missing notifications / enabled comma-dangle option in eslint / fixes #44

--- src/localizationContent.js
+++ src/localizationContent.js
@@ -7,16 +7,16 @@ const resources = {
     pl: {
         app: {
             appName: 'Corpo food',
-            uniqueValueProposition: ' orderuj food bez fakapu'
+            uniqueValueProposition: ' orderuj food bez fakapu',
         },
         dashboard: {
             noOrdersYet: 'Nikt jeszcze nie sfokusował się na czelendża - bądź pierwszy',
-            addOrder: 'DODAJ ORDER'
+            addOrder: 'DODAJ ORDER',
         },
         orderTile: {
             deadline: 'Deadlajn:',
             author: 'Fokusuje się:',
-            alreadyOrdered: 'os. już zamówiły'
+            alreadyOrdered: 'os. już zamówiły',
         },
         newOrderForm: {
             restaurant: 'Lokal',
@@ -35,29 +35,29 @@ const resources = {
                 provideMenuLink: 'Proszę podać odnośnik do menu',
                 provideAuthor: 'Proszę podać autora zamówienia',
                 provideValidDeliveryCost: 'Podaj poprawny koszt dostawy',
-                provideValidExtraCostPerMeal: 'Podaj poprawny koszt do każdego zamówienia'
-            }
+                provideValidExtraCostPerMeal: 'Podaj poprawny koszt do każdego zamówienia',
+            },
         },
         schemaValidation: {
             order: {
                 deadline: {
                     required: 'Nie zapomnij o deadljnie!',
-                    min: 'Dedlajn nie może być w przeszłości'
+                    min: 'Dedlajn nie może być w przeszłości',
                 },
                 deliveryTime: {
                     required: 'Czas dostawy jest wymagany!',
-                    min: 'Czas dostawy nie może być w przeszłości'
+                    min: 'Czas dostawy nie może być w przeszłości',
                 },
                 restaurant: {
-                    required: 'Nazwa lokalu jest wymagana!'
+                    required: 'Nazwa lokalu jest wymagana!',
                 },
                 menu: {
-                    required: 'Menu jest wymagane! Wrzuć linka do menu.'
+                    required: 'Menu jest wymagane! Wrzuć linka do menu.',
                 },
                 author: {
-                    required: 'Podaj imię!'
-                }
-            }
+                    required: 'Podaj imię!',
+                },
+            },
         },
         orderDetails: {
             backToDashboard: 'Wróć na dashboard',
@@ -67,38 +67,51 @@ const resources = {
             foodExecptedAt: 'Chcemy jeść o',
             extraCostPerMeal: 'Dodatkowo do każdego zamówienia',
             deliveryCost: 'Dostawa kosztuje',
-            descriptionHeader: 'Opis'
+            descriptionHeader: 'Opis',
         },
         mealList: {
             whoIsOrdering: 'Zamawiają razem',
             who: 'kto',
             what: 'co',
-            howMuch: 'za ile'
+            howMuch: 'za ile',
         },
         signUpForMeal: {
             whatDoYouWant: 'co chcesz?',
             menuPrice: 'cena z menu',
-            action: 'fokusuj'
+            action: 'fokusuj',
         },
         setUsername: {
             username: 'nazwa użytkownika',
             provideUsername: 'podaj nazwę użytkownika',
-            randomUsername: 'LOSOWA NAZWA'
-        }
+            randomUsername: 'LOSOWA NAZWA',
+        },
+        orderManagement: {
+            managerBadge: 'MENADŻER',
+            states: {
+                ordered: 'Zamówione!',
+            },
+        },
+        notifications: {
+            manager: {
+                title: 'Zamówienie gotowe, ludzie mogą bez fakapu zamawiać jedzenie',
+                introToLink: 'Oto Twój permalink pod którym możesz się fokusować na zamówienie:',
+                betterSaveIt: 'Lepiej uniknij fakapu i gdzieś zapisz ten link, bo nie pokażemy Ci go więcej!',
+            },
+        },
     },
     en: {
         app: {
             appName: 'Corpo food',
-            uniqueValueProposition: ' order food without fuckups'
+            uniqueValueProposition: ' order food without fuckups',
         },
         dashboard: {
             noOrdersYet: 'Nobody is on the task yet - be first',
-            addOrder: 'ADD ORDER'
+            addOrder: 'ADD ORDER',
         },
         orderTile: {
             deadline: 'Deadline:',
             author: 'On the task:',
-            alreadyOrdered: 'have already ordered'
+            alreadyOrdered: 'have already ordered',
         },
         newOrderForm: {
             restaurant: 'Restaurant',
@@ -117,32 +130,32 @@ const resources = {
                 provideMenuLink: 'Provide menu link',
                 provideAuthor: 'Provide author',
                 provideValidDeliveryCost: 'Provide valid delivery cost',
-                provideValidExtraCostPerMeal: 'Provide valid extra cost per meal'
-            }
+                provideValidExtraCostPerMeal: 'Provide valid extra cost per meal',
+            },
         },
         schemaValidation: {
             order: {
                 deadline: {
                     required: 'Dont forget the deadline!',
-                    min: 'Deadline must not be in the past'
+                    min: 'Deadline must not be in the past',
                 },
                 deliveryTime: {
                     required: 'Delivery time is required!',
-                    min: 'Delivery time must not be in the past'
+                    min: 'Delivery time must not be in the past',
                 },
                 restaurant: {
-                    required: 'Provide name for the restaurant'
+                    required: 'Provide name for the restaurant',
                 },
                 menu: {
-                    required: 'Add menu link to make our lives easier'
+                    required: 'Add menu link to make our lives easier',
                 },
                 author: {
-                    required: 'Who are you?'
+                    required: 'Who are you?',
                 },
                 accessCode: {
-                    required: 'Access code is required'
-                }
-            }
+                    required: 'Access code is required',
+                },
+            },
         },
         orderDetails: {
             backToDashboard: 'Back to dashboard',
@@ -152,38 +165,38 @@ const resources = {
             foodExecptedAt: 'We want food at',
             extraCostPerMeal: 'Extra cost per meal',
             deliveryCost: 'Delivery cost',
-            descriptionHeader: 'Description'
+            descriptionHeader: 'Description',
         },
         mealList: {
             whoIsOrdering: 'Already ordering together',
             who: 'who',
             what: 'what',
-            howMuch: 'how much'
+            howMuch: 'how much',
         },
         signUpForMeal: {
             whatDoYouWant: 'meal name',
             menuPrice: 'menu price',
-            action: 'let\'s go'
+            action: 'let\'s go',
         },
         setUsername: {
             username: 'Username',
             provideUsername: 'provide a username',
-            randomUsername: 'RANDOM USERNAME'
+            randomUsername: 'RANDOM USERNAME',
         },
         orderManagement: {
             managerBadge: 'MANAGER',
             states: {
-                ordered: 'Food ordered'
-            }
+                ordered: 'Food ordered',
+            },
         },
         notifications: {
             manager: {
-                title: 'Order is ready, here is your link to manage it',
+                title: 'Order is ready, people can sign up for food!',
                 introToLink: 'Here is your permanent link to a page where you can manage this order:',
-                betterSaveIt: 'You should probably save this link somewhere as we will never give it to you again!'
-            }
-        }
-    }
+                betterSaveIt: 'You should probably save this link somewhere as we will never give it to you again!',
+            },
+        },
+    },
 };
 
 module.exports.resources = resources;
